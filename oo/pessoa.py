3e1f9df8c9ddcd41ab8d8f414f52f2dfd7f3fcb2
programer-Glei <159723Natalia*/dfre0>
2020-12-14 20:04:34 -0300

    Criadoa classe carro e seus atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,25 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=33):
+    def __init__(self, nome=None, idade=33, *filhos):
         self.nome = nome
         self.idade = idade
+        self.filhos = filhos
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Gleibson', 36)
+    pessoa = []
+    joaquim = Pessoa('Joaquim')
+    maria = Pessoa('Maria')
+    jose = Pessoa('Jose',40)
+    pessoa.append(joaquim)
+    pessoa.append(maria)
+    pessoa.append(jose)
+    p = Pessoa('Gleibson', 36, pessoa)
     print(p.cumprimentar())
     print(p.nome)
     print(p.idade)
+    for filo in p.filhos:
+        for achei in filo:
+            print(f'nome {achei.nome} e idade {achei.idade}')
